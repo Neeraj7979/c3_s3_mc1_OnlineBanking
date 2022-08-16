e0917973a0ed63e276b07bdd819b53eee13a7450
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-16 22:58:34 +0530

    Declare attributes of the class SavingAccount.

---
+++ src/SavingAccount.java
@@ -0,0 +1,14 @@
+public class SavingAccount {
+  private String accountHolderName;
+  private int accountNumber;
+  private String accountOpeningDate;
+  private boolean accountStatus;
+  private double availableBalance;
+  private double totalBalance;
+  private boolean internetAndMobileBankingEnabled;
+  private String modeOfOperation;
+
+
+
+
+}
