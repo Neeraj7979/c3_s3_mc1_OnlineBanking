d8b0ca10ba6efc1fc7937bcb5360200b35dfd823
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-16 23:17:16 +0530

    Defining method for retrieving,credit and debit of balance.

--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -8,6 +8,64 @@ public class SavingAccount {
   private boolean internetAndMobileBankingEnabled;
   private String modeOfOperation;
 
+  public String getAccountHolderName(){
+    return accountHolderName;
+  }
+  public void setAccountHolderName(String accountHolderName){
+    this.accountHolderName=accountHolderName;
+  }
+  public int getAccountNumber(){
+    return accountNumber;
+  }
+  public void setAccountNumber(int accountNumber){
+    this.accountNumber=accountNumber;
+  }
+  public String getAccountOpeningDate(){
+    return accountOpeningDate;
+  }
+  public void setAccountOpeningDate(String accountOpeningDate){
+    this.accountOpeningDate=accountOpeningDate;
+  }
+  public boolean isAccountStatus(){
+    return accountStatus;
+  }
+  public void setAccountStatus(boolean accountStatus){
+    this.accountStatus=accountStatus;
+  }
+  public double getAvailableBalance(){
+    return availableBalance;
+  }
+  public void setAvailableBalance(double availableBalance){
+    this.availableBalance=availableBalance;
+  }
+  public double getTotalBalance(){
+    return totalBalance;
+  }
+  public void setTotalBalance(double totalBalance){
+    this.totalBalance=totalBalance;
+  }
+  public boolean isInternetAndMobileBankingEnabled(){
+    return internetAndMobileBankingEnabled;
+  }
+  public void setInternetAndMobileBankingEnabled(boolean internetAndMobileBankingEnabled){
+    this.internetAndMobileBankingEnabled=internetAndMobileBankingEnabled;
+  }
+  public String getModeOfOperation(){
+    return modeOfOperation;
+  }
+  public void setModeOfOperation(String modeOfOperation){
+    this.modeOfOperation=modeOfOperation;
+  }
+
+  public double retrieveBalance(){
+    return totalBalance;
+  }
+  public double debitAmount(double amount){
+    return totalBalance=availableBalance-amount;
+  }
+  public double creditAmount(double amount){
+    return totalBalance=availableBalance+amount;
+  }
 
 
 
